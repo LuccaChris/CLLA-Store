e1b6325589e0662d95095a36a479b1223a44fe91
Christian <112213507+LuccaChris@users.noreply.github.com>
2025-08-07 18:16:12 -0300

    feat: atualizar ícone do favicon e adicionar Font Awesome ao Navbar

--- CLLA-Store/src/components/Navbar.tsx
+++ CLLA-Store/src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMobileButton } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <nav className="bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold text-black tracking-tight">
-          Premium <span className="text-zinc-600">imports</span>
+          Premium <span className="text-zinc-600">imports</span><FontAwesomeIcon icon={faMobileButton} size="1x" />
         </Link>
         <div className="space-x-6 text-sm font-medium">
           <Link to="/products" className="hover:text-blue-500 transition">Produtos</Link>
--- CLLA-Store/src/pages/Products.tsx
+++ CLLA-Store/src/pages/Products.tsx
@@ -3,11 +3,11 @@ export default function Products() {
     <div className="max-w-6xl mx-auto px-4 py-8">
       <h2 className="text-2xl font-semibold mb-6">Produtos em destaque</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {[1, 2, 3, 4, 5, 6].map((i) => (
+        {["IPhone 16 Pro MAX", "S24 +","S25 +","IPhone 15","IPhone 15 Pro MAX","IPhone 16",].map((i) => (
           <div key={i} className="bg-white border rounded-lg shadow hover:shadow-md transition p-4">
-            <div className="h-40 bg-gray-200 rounded mb-4"></div>
-            <h3 className="text-lg font-semibold">Produto {i}</h3>
-            <p className="text-sm text-gray-500 mb-2">Descrição breve do produto {i}.</p>
+            <div className="h-40 bg-blue-200 rounded mb-4"></div>
+            <h3 className="text-lg font-semibold">{i}</h3>
+            <p className="text-sm text-gray-500 mb-2">Descrição {i}.</p>
             <button className="w-full bg-black text-white py-2 rounded hover:bg-gray-800 transition">Adicionar ao carrinho</button>
           </div>
         ))}
