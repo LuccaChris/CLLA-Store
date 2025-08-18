3b43d602832f69a3690ec043fb8119c38f7697da
Christian <112213507+LuccaChris@users.noreply.github.com>
2025-08-18 18:10:22 -0300

    Implement code changes to enhance functionality and improve performance

--- CLLA-Store/src/pages/Products.tsx
+++ CLLA-Store/src/pages/Products.tsx
@@ -5,9 +5,9 @@ export default function Products() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {["IPhone 16 Pro MAX", "S24 +","S25 +","IPhone 15","IPhone 15 Pro MAX","IPhone 16",].map((i) => (
           <div key={i} className="bg-white border rounded-lg shadow hover:shadow-md transition p-4">
-            <div className="h-40 bg-blue-200 rounded mb-4"></div>
-            <h3 className="text-lg font-semibold">{i}</h3>
-            <p className="text-sm text-gray-500 mb-2">Descrição {i}.</p>
+            <div className="h-40 bg-teal-900 rounded mb-4"><img src={`/${i}.jpg`} alt={i} /></div>
+            <h3 className=" text-lg text-white font-semibold">{i}</h3>
+            <p className="text-sm text-white ml-1 mb-2">Descrição {i}.</p>
             <button className="w-full bg-black text-white py-2 rounded hover:bg-gray-800 transition">Adicionar ao carrinho</button>
           </div>
         ))}
