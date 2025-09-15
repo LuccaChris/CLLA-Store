78fc3e9880a74c3a6dd5202d10ae7e9eb231cc2f
Christian <112213507+LuccaChris@users.noreply.github.com>
2025-09-15 08:46:05 -0300

    Refatorar estrutura do App e adicionar configuração do Vercel

--- CLLA-Store/src/App.tsx
+++ CLLA-Store/src/App.tsx
@@ -1,28 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer' // Importa o footer
-import Home from './pages/Home'
-import Products from './pages/Products'
-import Cart from './pages/Cart'
-import Checkout from './pages/Checkout'
-import Success from './pages/Success'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Success from "./pages/Success";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/success" element={<Success />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
